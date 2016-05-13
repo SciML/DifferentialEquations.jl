819ce60fac687fdc07485fac827cdbba78af939f
Chris Rackauckas <contact@chrisrackauckas.com>
2016-05-13 09:42:02 -0700

    fixing adaptive tools for after build checks

--- src/fem/adaptiveTools.jl
+++ src/fem/adaptiveTools.jl
@@ -34,11 +34,11 @@ mark(elem,eta,theta;method="L2")
 function mark(elem,eta,theta;method="L2")
   NT = size(elem,1); isMark = false(NT,1)
   if method == "Max"
-    isMark(eta>theta*max(eta))=1
+    isMark[eta>theta*max(eta)]=1
   elseif method == "COARSEN"
-    isMark(eta<theta*max(eta))=1
+    isMark[eta<theta*max(eta)]=1
   elseif method == "L2"
-    [sortedEta,idx] = sort(eta.^2,rev=true)
+    sortedEta,idx = sort(eta.^2,rev=true)
     x = cumsum(sortedEta)
     isMark[idx[x < theta* x[NT]]] = 1
     isMark[idx[1]] = 1
@@ -69,12 +69,12 @@ function bisect(node,elem;markedElem=1:size(elem,1),bdFlag=[])
   # Add new nodes
   isCutEdge = false(NE,1)
   while sum(markedElem)>0
-      isCutEdge(elem2edge(markedElem,1)) = true
+      #isCutEdge[elem2edge(markedElem,1)] = true
       refineNeighbor = neighbor(markedElem,1)
       markedElem = refineNeighbor(~isCutEdge(elem2edge(refineNeighbor,1)))
   end
   edge2newNode = zeros(Int64,NE,1)
-  edge2newNode(isCutEdge) = N+1:N+sum(isCutEdge)
+  edge2newNode[isCutEdge] = N+1:N+sum(isCutEdge)
   HB = zeros(Int64,sum(isCutEdge),3)
   HB[:,1] = edge2newNode(isCutEdge)
   HB[:,[2 3]] = edge(isCutEdge,[1 2])
@@ -85,15 +85,15 @@ function bisect(node,elem;markedElem=1:size(elem,1),bdFlag=[])
   for k = 1:2
       t = find(edge2newNode(elem2edge(:,1))>0)
       newNT = length(t)
-      if (newNT == 0), break; end
-      L = t; R = NT+1:NT+newNT;
+      if (newNT == 0) break; end
+      L = t; R = NT+1:NT+newNT
       p1 = elem(t,1); p2 = elem(t,2); p3 = elem(t,3)
       p4 = edge2newNode(elem2edge(t,1))
-      elem[L,:] = [p4, p1, p2]
-      elem[R,:] = [p4, p3, p1]
-  	if nargin==4 && ~isempty(bdFlag) % Refine boundary edges
-     		bdFlag[R,[1 3]] = bdFlag[t,[2 1]]
-     		bdFlag[L,[1 2]] = bdFlag[t,[3 1]]
+      elem[L,:] = [p4 p1 p2]
+      elem[R,:] = [p4 p3 p1]
+    if nargin==4 && ~isempty(bdFlag) # Refine boundary edges
+     		#bdFlag[R [1 3]] = bdFlag[t [2 1]]
+     		#bdFlag[L [1 2]] = bdFlag[t [3 1]]
           bdFlag[L,3] = 0
       else
           bdFlag = []
