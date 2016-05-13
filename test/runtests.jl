b74272fbf432e031061e345bc3a76a1372d84738
Chris Rackauckas <contact@chrisrackauckas.com>
2016-05-12 20:57:12 -0700

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,15 @@
+#!/usr/bin/env julia
 using DiffEq
 using Base.Test
 
-# write your own tests here
+# Run tests
+include("femHeatAnimationTest.jl")
+include("femHeatDtTests.jl")
+include("femHeatDXtests.jl")
+include("femHeatMethodTest.jl")
+include("femHeatNonlinearMethodsTest.jl")
+include("femPoissonConvTest.jl")
+include("femPoissonNonlinearTest.jl")
+include("femStochasticHeatAnimationTest.jl")
+include("femStochasticPoissonSolo.jl")
+include("introductionExample.jl")
