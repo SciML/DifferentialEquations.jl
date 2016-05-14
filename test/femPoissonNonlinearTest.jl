9358a9f9ee1a0a68198f3b0f403bf50f22cdd3c1
Chris Rackauckas <contact@chrisrackauckas.com>
2016-05-14 12:18:08 -0700

    improve tests

--- test/femPoissonNonlinearTest.jl
+++ test/femPoissonNonlinearTest.jl
@@ -9,7 +9,6 @@ pdeProb = poissonProblemExample_birthdeath()
 
 res = fem_solvepoisson(femMesh::FEMmesh,pdeProb::PoissonProblem,solver="GMRES")
 
-Plots.gr()
 Plots.surface(femMesh.node[:,1],femMesh.node[:,2],res.u,zlim=(0,2),cbar=false)
 
 #Returns true if computed solution is homogenous near 2
