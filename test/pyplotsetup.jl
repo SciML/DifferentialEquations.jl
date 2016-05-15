e8d7fa5217b47b7b90f2958b5dd5609844f958a9
Chris Rackauckas <contact@chrisrackauckas.com>
2016-05-15 00:26:49 -0700

    appveyor updates

--- test/pyplotsetup.jl
+++ test/pyplotsetup.jl
@@ -1,6 +1,10 @@
 ## Disconnect from the Python Environment
 ## And use what's in the Conda package
 
+Pkg.add("WinRPM")
+using WinRPM
+WinRPM.update()
+
 ENV["PYTHON"]=""
 Pkg.build("PyCall")
 using PyPlot
