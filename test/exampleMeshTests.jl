b5f2282e3ab4909692c5c750bc48ffb81a279f7f
Chris Rackauckas <contact@chrisrackauckas.com>
2016-05-16 08:50:48 -0700

    More tests

---
+++ test/exampleMeshTests.jl
@@ -0,0 +1,21 @@
+###Example Meshes
+
+mesh = meshExample_bunny()
+
+mesh = meshExample_flowpastcylindermesh()
+
+mesh = meshExample_lakemesh()
+
+mesh = meshExample_Lshapemesh()
+
+mesh = meshExample_Lshapeunstructure()
+
+mesh = meshExample_oilpump()
+
+mesh = meshExample_wavymesh()
+
+mesh = meshExample_wavyperturbmesh()
+
+showmesh(mesh)
+
+true
