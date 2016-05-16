e7f0d86a4310a00f6802ac1e5ec8ab9ed34705c2
Chris Rackauckas <contact@chrisrackauckas.com>
2016-05-15 21:11:25 -0700

    Fix up example mesh loading

--- src/examples/exampleMeshes.jl
+++ src/examples/exampleMeshes.jl
@@ -1,25 +1,25 @@
 ###Example Meshes
 
 "meshExample_bunny() : Returns a 3D SimpleMesh."
-meshExample_bunny() = load("src/examples/prebuiltMeshes.jld","bunny")
+meshExample_bunny() = JLD.load("src/examples/prebuiltMeshes.jld","bunny")
 
 "meshExample_flowpastcylindermesh() : Returns a 2D SimpleMesh."
-meshExample_flowpastcylindermesh() = load("src/examples/prebuiltMeshes.jld","flowpastcylindermesh")
+meshExample_flowpastcylindermesh() = JLD.load("src/examples/prebuiltMeshes.jld","flowpastcylindermesh")
 
 "meshExample_lakemesh() : Returns a 2D SimpleMesh."
-meshExample_lakemesh() = load("src/examples/prebuiltMeshes.jld","lakemesh")
+meshExample_lakemesh() = JLD.load("src/examples/prebuiltMeshes.jld","lakemesh")
 
 "meshExample_Lshapemesh() : Returns a 2D SimpleMesh."
-meshExample_Lshapemesh() = load("src/examples/prebuiltMeshes.jld","Lshapemesh")
+meshExample_Lshapemesh() = JLD.load("src/examples/prebuiltMeshes.jld","Lshapemesh")
 
 "meshExample_Lshapeunstructure() : Returns a 2D SimpleMesh."
-meshExample_Lshapeunstructure() = load("src/examples/prebuiltMeshes.jld","Lshapeunstructure")
+meshExample_Lshapeunstructure() = JLD.load("src/examples/prebuiltMeshes.jld","Lshapeunstructure")
 
 "meshExample_oilpump() : Returns a 3D SimpleMesh."
-meshExample_oilpump() = load("src/examples/prebuiltMeshes.jld","oilpump")
+meshExample_oilpump() = JLD.load("src/examples/prebuiltMeshes.jld","oilpump")
 
 "meshExample_wavymesh() : Returns a 2D SimpleMesh."
-meshExample_wavymesh() = load("src/examples/prebuiltMeshes.jld","wavymesh")
+meshExample_wavymesh() = JLD.load("src/examples/prebuiltMeshes.jld","wavymesh")
 
 "meshExample_wavyperturbmesh() : Returns a 3D SimpleMesh."
-meshExample_wavyperturbmesh() = load("src/examples/prebuiltMeshes.jld","wavyperturbmesh")
+meshExample_wavyperturbmesh() = JLD.load("src/examples/prebuiltMeshes.jld","wavyperturbmesh")
