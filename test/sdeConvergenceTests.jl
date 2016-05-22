d28174bd4d4d0c160e8b2254665abe3cb86abba3
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-05-22 05:19:14 -0700

    set random seed for tests

--- test/sdeConvergenceTests.jl
+++ test/sdeConvergenceTests.jl
@@ -1,4 +1,5 @@
 using DifferentialEquations
+srand(100)
 Δts = 1.//2.^(10:-1:4) #14->7 good plot
 
 prob = waveSDEExample()
@@ -19,6 +20,6 @@ convsim = testConvergence(Δts,prob,numMonte=Int(1e1),alg="EM")
 convsim2 = testConvergence(Δts,prob,numMonte=Int(1e1),alg="RKMil")
 convsim3 = testConvergence(Δts,prob,numMonte=Int(1e1),alg="SRI")
 convsim4 = testConvergence(Δts,prob,numMonte=Int(1e1),alg="SRA")
-bool3 = abs(convsim.𝒪est["l2"]-1) + abs(convsim2.𝒪est["l∞"]-1) + abs(convsim3.𝒪est["final"]-2) + abs(convsim4.𝒪est["final"]-2) <.5 #High tolerance since low Δts for testing!
+bool3 = abs(convsim.𝒪est["l2"]-1) + abs(convsim2.𝒪est["l∞"]-1) + abs(convsim3.𝒪est["final"]-2) + abs(convsim4.𝒪est["final"]-2) <.2 #High tolerance since low Δts for testing!
 
 bool1 && bool2 && bool3
