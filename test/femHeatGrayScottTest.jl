5fef29bc68db4d7566258bfab673b46b19886115
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-05-31 08:22:38 -0700

    Remove CheckImports dependence. Tests should pass.

--- test/femHeatGrayScottTest.jl
+++ test/femHeatGrayScottTest.jl
@@ -4,14 +4,14 @@
 using DifferentialEquations, Plots
 
 #Define a parabolic problem
-T = 1000
-Δx = 1//2^(3)
-Δt = 1//2^(8)
+T = 100
+Δx = 1//2^(4)
+Δt = 1//2^(14)
 femMesh = parabolic_squaremesh([0 1 0 1],Δx,Δt,T,"Neumann")
-prob = heatProblemExample_grayscott()
+prob = heatProblemExample_grayscott(D = [1 .0001])
 
 sol = solve(femMesh::FEMmesh,prob::HeatProblem,alg="Euler",fullSave=true,saveSteps=1)
 
-plot(sol,plottrue=false,zlim=(0,20),cbar=false)
+plot(sol,plottrue=false,zlim=(0,1),cbar=false)
 gui()
-#CFLμ(Δt,Δx)
+CFLμ(Δt,Δx)
