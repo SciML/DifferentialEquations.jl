a133be08d11b38eefd837556be5f9d2810c13888
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-05-31 10:59:08 -0700

    gierer meinhardt works

--- test/femHeatGiererMeinhardtTest.jl
+++ test/femHeatGiererMeinhardtTest.jl
@@ -1,18 +1,13 @@
-######
-##FEM Heat Nonlinear Test
-######
 using DifferentialEquations, Plots
 
 #Define a parabolic problem
-T = 10
-Δx = 1//2^(4)
+T = 40000
+Δx = 1//2^(3)
 Δt = 1//2^(9)
 femMesh = parabolic_squaremesh([0 1 0 1],Δx,Δt,T,"Neumann")
-prob = heatProblemExample_grayscott(;ρ=.3,k=.62,D=[1e-2 .5e-2])
+prob = heatProblemExample_gierermeinhardt()
 
 sol = solve(femMesh::FEMmesh,prob::HeatProblem,alg="Euler",fullSave=true,saveSteps=1000)
 
-plot(sol,plottrue=false,zlim=(0,1),cbar=false)
+plot(sol,plottrue=false,zlim=(0,20),cbar=false)
 gui()
-CFLμ(Δt,Δx)
-#animate(sol::FEMSolution;zlim=(0,3),cbar=false)
