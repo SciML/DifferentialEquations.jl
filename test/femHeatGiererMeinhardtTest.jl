8c37dfaf8b1fd286d48bf51d4c5a94c2a32d12af
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-06-01 07:25:06 -0700

    GM heatmaps, adaptive ODE

--- test/femHeatGiererMeinhardtTest.jl
+++ test/femHeatGiererMeinhardtTest.jl
@@ -2,7 +2,8 @@ using DifferentialEquations, Plots
 
 #Define a parabolic problem
 T = .5
-Δx = 1//2^(4)
+k = 4
+Δx = 1//2^(k)
 Δt = 1//2^(10)
 femMesh = parabolic_squaremesh([0 1 0 1],Δx,Δt,T,"Neumann")
 #prob = heatProblemExample_gierermeinhardt(a=1,α=1,D=[0.01 1.0],ubar=1,vbar=0,β=10) #Saddle
@@ -14,3 +15,21 @@ plot(sol,plottrue=false,zlim=(0,10),cbar=false)
 gui()
 
 @linux? animate(sol,zlims=[(0,10),(0,2)],cbar=false) : println("Animation only works with ImageMagick installation, disabled on osx for testing")
+
+Plots.heatmap(vec(reshape(sol.femMesh.node[:,1],2^(4)+1,2^(k)+1)[1,:]),
+              vec(reshape(sol.femMesh.node[:,2],2^(4)+1,2^(k)+1)[:,1]),
+              reshape(sol.timeSeries[1][1],2^(4)+1,2^(k)+1))
+zlims=[(0,10),(0,2)]
+cbar = false
+atomLoaded = isdefined(Main,:Atom)
+Plots.pyplot(reuse=true,size=(1500,750))
+@gif for j=1:length(sol.timeSeries[1])
+  ps = Any[]
+  for i=1:sol.prob.numVars
+    push!(ps,Plots.heatmap(vec(reshape(sol.femMesh.node[:,1],2^(4)+1,2^(k)+1)[1,:]),
+                  vec(reshape(sol.femMesh.node[:,2],2^(4)+1,2^(k)+1)[:,1]),
+                  reshape(sol.timeSeries[i][j],2^(4)+1,2^(k)+1),zlim=zlims[i],cbar=cbar))
+  end
+  plot(ps...)
+  atomLoaded ? Main.Atom.progress(j/length(sol.timeSeries[1])) : nothing #Use Atom's progressbar if loaded
+end
