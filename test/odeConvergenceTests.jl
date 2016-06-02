782648ae0e63db2e0dbf12233e392cd4c764dbdc
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-06-02 07:45:56 -0700

    Implicit solvers

--- test/odeConvergenceTests.jl
+++ test/odeConvergenceTests.jl
@@ -5,6 +5,7 @@ prob = twoDimlinearODEExample()
 println("Convergence Test on Linear")
 Δts = 1.//2.^(8:-1:4)
 
+println("Special RKs")
 sim = testConvergence(Δts,prob,alg="Euler")
 bool1 = abs(sim.𝒪est["final"]-1) < 0.1
 sim2 = testConvergence(Δts,prob,alg="Midpoint")
@@ -14,7 +15,7 @@ bool3 = abs(sim3.𝒪est["l∞"]-4) < 0.1
 tab = constructHuen()
 sim4 = testConvergence(Δts,prob,alg="ExplicitRK",tableau=tab)
 bool4 = abs(sim4.𝒪est["l∞"]-2) < 0.1
-
+println("Other RKs")
 tab = constructRalston()
 sim5 = testConvergence(Δts,prob,alg="ExplicitRK",tableau=tab)
 bool5 = abs(sim5.𝒪est["l∞"]-2) < 0.1
@@ -23,6 +24,7 @@ tab = constructBogakiShampine()
 sim6 = testConvergence(Δts,prob,alg="ExplicitRK",tableau=tab)
 bool6 = abs(sim6.𝒪est["l∞"]-3) < 0.1
 
+println("Higher Order")
 Δts = 1.//2.^(7:-1:3)
 tab = constructRKF()
 sim7 = testConvergence(Δts,prob,alg="ExplicitRK",tableau=tab)
@@ -36,9 +38,25 @@ tab = constructCashKarp()
 sim9 = testConvergence(Δts,prob,alg="ExplicitRK",tableau=tab)
 bool9 = abs(sim9.𝒪est["l∞"]-5) < 0.1
 
+println("Super High Order")
 #Need to make larger or else below machine ϵ
 Δts = 1./2.0.^(2:-1:-2)
 tab = constructRKF8()
 sim10 = testConvergence(Δts,prob,alg="ExplicitRK",tableau=tab)
 bool10 = abs(sim10.𝒪est["l∞"]-8) < 0.1
-bool1 && bool2 && bool3 && bool4 && bool5 && bool6 && bool7 && bool8 && bool9 && bool10
+superbool1 = bool1 && bool2 && bool3 && bool4 && bool5 && bool6 && bool7 && bool8 && bool9 && bool10
+
+### Stiff Solvers
+
+println("Convergence Test on Stiff")
+Δts = 1.//2.^(8:-1:4)
+
+sim11 = testConvergence(Δts,prob,alg="ImplicitEuler")
+bool11 = abs(sim11.𝒪est["final"]-1) < 0.1
+
+sim12 = testConvergence(Δts,prob,alg="Trapezoid")
+bool12 = abs(sim12.𝒪est["final"]-2) < 0.1
+
+superbool2 = bool11 && bool12
+
+superbool1 && superbool2
