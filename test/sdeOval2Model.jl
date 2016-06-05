f39baf93e3760c15250fd91cb5185a5273cedee7
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-06-05 10:26:58 -0700

    Oval2 Model with large fluctuations success!

--- test/sdeOval2Model.jl
+++ test/sdeOval2Model.jl
@@ -1,10 +1,15 @@
-using DifferentialEquations, Plots, EllipsisNotation
+using DifferentialEquations, Plots, EllipsisNotation, JLD
 srand(100)
 prob = oval2ModelExample()
 
-sol =solve(prob::SDEProblem,[0;10],Δt=1//2^(10),fullSave=true,alg="SRI",adaptiveAlg="RSwM3",adaptive=true,progressBar=true,saveSteps=1000,abstol=1e-6,reltol=1e-4)
+sol =solve(prob::SDEProblem,[0;500],Δt=1//2^(10),fullSave=true,alg="SRI",adaptiveAlg="RSwM3",adaptive=true,progressBar=true,saveSteps=1,abstol=1e-6,reltol=1e-4)
 evim = Any[sol.uFull[..,16];sol.uFull[..,17]]
 p1 = plot(sol.tFull,sol.uFull[..,16],top_margin=50px,title="Ecad")
 p2 = plot(sol.tFull,sol.uFull[..,17],top_margin=50px,title="Vim")
 plot(p1,p2)
 gui()
+
+plot(sol.tFull[1:end-1],diff(sol.tFull)/1000,xguide="time",yguide="Average Dt over 1000 accepted steps")
+gui()
+
+save("Oval2Solution.jld","sol",sol,"prob",prob)
