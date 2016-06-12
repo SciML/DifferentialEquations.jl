c7b8ab7ec7dbf0af09666d9009e90a797df9d32d
ChrisRackauckas <me@chrisrackauckas.com>
2016-06-12 11:57:31 -0700

    Pulled from private branch, scrubbed private code

--- src/sde/sdeSolvers.jl
+++ src/sde/sdeSolvers.jl
@@ -29,29 +29,50 @@
   if numVars == 1
     atemp = 0.0
     btemp = 0.0
+    E₂temp= 0.0
   else
     atemp = zeros(size(u))
     btemp = zeros(size(u))
+    E₂    = zeros(size(u))
+    E₁temp= zeros(size(u))
   end
   for i = 1:length(α)
-    @inbounds atemp += α[i]*f(H0[..,i],t+c₀[i]*Δt)
-    @inbounds btemp += (β₁[i]*ΔW + β₂[i]*chi1 + β₃[i]*chi2 + β₄[i]*chi3).*σ(H1[..,i],t+c₁[i]*Δt)
+    @inbounds ftemp = f(H0[..,i],t+c₀[i]*Δt)
+    @inbounds atemp += α[i]*ftemp
+    @inbounds btemp += (β₁[i]*ΔW + β₂[i]*chi1).*σ(H1[..,i],t+c₁[i]*Δt)
+    @inbounds E₂    += (β₃[i]*chi2 + β₄[i]*chi3).*σ(H1[..,i],t+c₁[i]*Δt)
+    if i<3 #1 or 2
+      E₁temp += ftemp
+    end
   end
-  u = u + Δt*atemp + btemp
+  E₁ = Δt*E₁temp
+
+  if adaptive
+    #No adaptivity until publication
+  else
+    u = u + Δt*atemp + btemp + E₂
+  end
 end
 
 @def SRIVectorized begin
   chi1 = .5*(ΔW.^2 - Δt)/sqΔt #I_(1,1)/sqrt(h)
   chi2 = .5*(ΔW + ΔZ/sqrt(3)) #I_(1,0)/h
   chi3 = 1/6 * (ΔW.^3 - 3*ΔW*Δt)/Δt #I_(1,1,1)/h
-  H0[:]=zeros(length(α))
-  H1[:]=zeros(length(α))
+  H0[:]=zeros(uType,4)
+  H1[:]=zeros(uType,4)
   for i = 1:length(α)
     H0temp = u + Δt*dot(vec(A₀[i,:]),f(H0,t + c₀*Δt)) + chi2*dot(vec(B₀[i,:]),σ(H1,t+c₁*Δt))
     H1[i]  = u + Δt*dot(vec(A₁[i,:]),f(H0,t + c₀*Δt)) + sqΔt*dot(vec(B₁[i,:]),σ(H1,t+c₁*Δt))
     H0[i] = H0temp
   end
-  u = u + Δt*dot(α,f(H0,t+c₀*Δt)) + dot(β₁*ΔW + β₂*chi1 + β₃*chi2 + β₄*chi3,σ(H1,t+c₁*Δt))
+  fVec = f(H0,t+c₀*Δt)
+  E₁ = Δt*(fVec[1]+fVec[2])
+  E₂ = dot(β₃*chi2 + β₄*chi3,σ(H1,t+c₁*Δt))
+  if adaptive
+    #No adaptivity until publication
+  else
+    u = u + Δt*dot(α,fVec) + dot(β₁*ΔW + β₂*chi1,σ(H1,t+c₁*Δt)) + E₂
+  end
 end
 
 @def SRAVectorized begin
@@ -60,7 +81,14 @@ end
   for i = 1:length(α)
     H0[i] = u + Δt*dot(vec(A₀[i,:]),f(H0,t + c₀*Δt)) + chi2*dot(vec(B₀[i,:]),σ(H0,t+c₁*Δt))
   end
-  u = u + Δt*dot(α,f(H0,t+c₀*Δt)) + dot(β₁*ΔW + β₂*chi2,σ(H0,t+c₁*Δt))
+  fVec = f(H0,t+c₀*Δt)
+  E₁ = Δt*(fVec[1]+fVec[2])
+  E₂ = dot(β₂*chi2,σ(H0,t+c₁*Δt))
+  if adaptive
+    #No adaptivity until publication
+  else
+    u = u + Δt*dot(α,f(H0,t+c₀*Δt)) + dot(β₁*ΔW,σ(H0,t+c₁*Δt)) + E₂
+  end
 end
 
 """
@@ -73,24 +101,36 @@ If not given, tspan defaults to [0,1]. If
 
 * fullSave: Saves the result at every saveSteps steps. Default is false.
 saveSteps: If fullSave is true, then the output is saved every saveSteps steps.
-* alg: String which defines the solver algorithm. Defult is "EM". Possibilities are:
+* alg: String which defines the solver algorithm. Defult is "SRI". Possibilities are:
   * "EM"- The Euler-Maruyama method.
   * "RKMil" - An explicit Runge-Kutta discretization of the strong Order 1.0 Milstein method.
   * "SRA" - The strong Order 1.5 method for additive SDEs due to Rossler.
-  * "SRI" - The strong Order 1.5 method for diagonal/scalar SDEs due to Rosser. Most efficient.
+  * "SRI" - The strong Order 1.5 method for diagonal/scalar SDEs due to Rossler. Most efficient.
 """
-function solve(prob::SDEProblem,tspan::AbstractArray=[0,1];Δt::Number=0,fullSave::Bool = false,saveSteps::Int = 1,alg::AbstractString="EM")
+function solve(prob::SDEProblem,tspan::AbstractArray=[0,1];Δt::Number=0,fullSave::Bool = false,
+              saveSteps::Int = 1,alg::AbstractString="SRI",adaptive=false,γ=2.0,
+              abstol=1e-3,reltol=1e-2,qmax=4,δ=1/6,maxIters::Int = round(Int,1e9),
+              Δtmax=nothing,Δtmin=nothing,
+              discardLength=1e-15,adaptiveAlg="RSwM3",progressBar=false,tType=typeof(Δt))
 
   @unpack prob: f,σ,u₀,knownSol,sol, numVars, sizeu
-
+  adaptive = false #No adaptivity until publication
   tspan = vec(tspan)
   if tspan[2]-tspan[1]<0 || length(tspan)>2
     error("tspan must be two numbers and final time must be greater than starting time. Aborting.")
   end
+  if Δtmax == nothing
+    Δtmax = tType((tspan[2]-tspan[1])/2)
+  end
+  if Δtmin == nothing
+    Δtmin = tType(1e-10)
+  end
+
+  uType = typeof(u₀)
+
   T = tspan[2]
   t = tspan[1]
-  u = float(u₀)
-  t = 0.0
+  u = u₀
   if numVars == 1
     W = 0.0
     Z = 0.0
@@ -101,57 +141,35 @@ function solve(prob::SDEProblem,tspan::AbstractArray=[0,1];Δt::Number=0,fullSav
 
   if fullSave
     uFull = GrowableArray(u)
-    tFull = Vector{Float64}(0)
+    tFull = Vector{tType}(0)
     WFull = GrowableArray(W)
     push!(tFull,t)
   end
 
-  #PreProcess
-  if Δt == 0
-    d₀ = norm(u₀./(abstol+u*reltol),2)
-    f₀ = f(u₀,t)
-    σ₀ = 2σ(u₀,t)
-    d₁ = norm(max(abs(f₀+2σ(u₀,t)),abs(f₀-2σ(u₀,t)))./(abstol+u*reltol),2)
-    if d₀ < 1e-5 || d₁ < 1e-5
-      Δt₀ = 1e-6
-    else
-      Δt₀ = 0.01*(d₀/d₁)
-    end
-    u₁ = u₀ + Δt₀*f₀
-    f₁ = f(u₀,t+Δt₀)
-    σ₁ = σ(u₀,t+Δt₀)
-    ΔσMax = max(abs(σ₀-σ₁),abs(σ₀+σ₁),abs(-σ₀-σ₁),abs(σ₁-σ₀))
-    d₂ = norm(max(f₁-f₀+ΔσMax,f₁-f₀-ΔσMax)./(abstol+u*reltol),2)/Δt₀
-    if max(d₁,d₂)<=1e-15
-      Δt₁ = max(1e-6,Δt₀*1e-3)
-    else
-      if !isdefined(Main,:order)
-        order = 1 #Convervative choice
-      end
-      Δt₁ = 10.0^(-(2+log10(max(d₁,d₂)))*2) #Order is assumed 1/2 for conservativity
-    end
-    Δt = min(100*Δt₀,Δt₁)
-  end
+  atomLoaded = isdefined(Main,:Atom)
 
-  sqΔt = sqrt(Δt)
+  #PreProcess
 
   if alg=="SRI"
     SRI = constructSRIW1()
     @unpack SRI: c₀,c₁,A₀,A₁,B₀,B₁,α,β₁,β₂,β₃,β₄
     if numVars == 1
-      H0 = Array{Float64}(length(α))
-      H1 = Array{Float64}(length(α))
+      H0 = Array{eltype(u)}(length(α))
+      H1 = Array{eltype(u)}(length(α))
     else
-      H0 = Array{Float64}(size(u)...,length(α))
-      H1 = Array{Float64}(size(u)...,length(α))
+      H0 = Array{eltype(u)}(size(u)...,length(α))
+      H1 = Array{eltype(u)}(size(u)...,length(α))
+    end
+    if adaptive
+      #No adaptivity until publication
     end
   elseif alg=="SRA"
     SRA = constructSRA1()
     @unpack SRA: c₀,c₁,A₀,B₀,α,β₁,β₂
     if numVars == 1
-      H0 = Array{Float64}(length(α))
+      H0 = Array{eltype(u)}(length(α))
     else
-      H0 = Array{Float64}(size(u)...,length(α))
+      H0 = Array{eltype(u)}(size(u)...,length(α))
     end
   end
   if numVars == 1
@@ -159,11 +177,36 @@ function solve(prob::SDEProblem,tspan::AbstractArray=[0,1];Δt::Number=0,fullSav
   else
     rands = ChunkedArray(randn,u)
   end
+
+  if alg == "Euler"
+    order = 0.5
+  elseif alg == "RKMil"
+    order = 1.0
+  else
+    order = 1.5
+  end
+
+  if Δt == 0
+    #No init Δt determiniation until publication.
+    error("User must supply Δt")
+  end
+
+  sqΔt = sqrt(Δt)
+
+  if adaptive && fullSave
+    ΔtFull = Vector{tType}(0)
+    push!(ΔtFull,Δt)
+  end
+
+  maxStackSize = 0
+  maxStackSize2= 0
   iter = 0
+  ΔW = sqΔt*next(rands) # Take one first
+  ΔZ = sqΔt*next(rands) # Take one first
+
   while t < T
     iter += 1
-    ΔW = sqΔt*next(rands)
-    ΔZ = sqΔt*next(rands)
+
     if alg=="EM"
       u = u + Δt.*f(u,t) + σ(u,t).*ΔW
     elseif alg=="RKMil"
@@ -178,15 +221,23 @@ function solve(prob::SDEProblem,tspan::AbstractArray=[0,1];Δt::Number=0,fullSav
     elseif alg=="SRI" && numVars == 1 #Only for explicit
       @SRIVectorized
     end
-    t = t + Δt
-    W = W + ΔW
-    Z = Z + ΔZ
-    if fullSave && iter%saveSteps==0
-      push!(uFull,u)
-      push!(tFull,t)
-      push!(WFull,W)
+    if adaptive
+      #No adaptivity until publication.
+    else # Non adaptive
+      t = t + Δt
+      W = W + ΔW
+      Z = Z + ΔZ
+      ΔW = sqΔt*next(rands)
+      ΔZ = sqΔt*next(rands)
+      if fullSave && iter%saveSteps==0
+        push!(uFull,u)
+        push!(tFull,t)
+        push!(WFull,W)
+      end
     end
+    (atomLoaded && progressBar) ? Main.Atom.progress(t/T) : nothing #Use Atom's progressbar if loaded
   end
+
 
   if knownSol
     uTrue = sol(u₀,t,W)
@@ -198,11 +249,22 @@ function solve(prob::SDEProblem,tspan::AbstractArray=[0,1];Δt::Number=0,fullSav
       WFull = copy(WFull)
       uFull = copy(uFull)
       solFull = copy(solFull)
-      return(SDESolution(u,uTrue,uFull=uFull,tFull=tFull,WFull=WFull,solFull=solFull))
+      if !adaptive
+        ΔtFull = Δt*ones(length(t))
+      end
+      return(SDESolution(u,uTrue,uFull=uFull,tFull=tFull,ΔtFull=ΔtFull,WFull=WFull,solFull=solFull,maxStackSize=maxStackSize))
     else
-      return(SDESolution(u,uTrue))
+      return(SDESolution(u,uTrue,maxStackSize=maxStackSize))
     end
   else #No known sol
-    return(SDESolution(u))
+    if fullSave
+      if !adaptive
+        ΔtFull = Δt*ones(length(t))
+      end
+      uFull = copy(uFull)
+      return(SDESolution(u,uFull=uFull,tFull=tFull,ΔtFull=ΔtFull,maxStackSize=maxStackSize))
+    else
+      return(SDESolution(u,maxStackSize=maxStackSize))
+    end
   end
 end
--- test/odeConvergenceTests.jl
+++ test/odeConvergenceTests.jl
@@ -4,50 +4,51 @@ srand(100)
 ## Convergence Testing
 println("Convergence Test on Linear")
 Δts = 1.//2.^(8:-1:4)
+testTol = 0.2
 
 println("Special RKs")
 sim = testConvergence(Δts,prob,alg="Euler")
-bool1 = abs(sim.𝒪est["final"]-1) < 0.1
+bool1 = abs(sim.𝒪est["final"]-1) < testTol
 sim2 = testConvergence(Δts,prob,alg="Midpoint")
-bool2 = abs(sim2.𝒪est["l∞"]-2) < 0.1
+bool2 = abs(sim2.𝒪est["l∞"]-2) < testTol
 sim3 = testConvergence(Δts,prob,alg="RK4")
-bool3 = abs(sim3.𝒪est["l∞"]-4) < 0.1
+bool3 = abs(sim3.𝒪est["l∞"]-4) < testTol
 tab = constructHuen()
 sim4 = testConvergence(Δts,prob,alg="ExplicitRK",tableau=tab)
-bool4 = abs(sim4.𝒪est["l∞"]-2) < 0.1
+bool4 = abs(sim4.𝒪est["l∞"]-2) < testTol
 println("Other RKs")
 tab = constructRalston()
 sim5 = testConvergence(Δts,prob,alg="ExplicitRK",tableau=tab)
-bool5 = abs(sim5.𝒪est["l∞"]-2) < 0.1
+bool5 = abs(sim5.𝒪est["l∞"]-2) < testTol
 
 tab = constructBogakiShampine()
 sim6 = testConvergence(Δts,prob,alg="ExplicitRK",tableau=tab)
-bool6 = abs(sim6.𝒪est["l∞"]-3) < 0.1
+bool6 = abs(sim6.𝒪est["l∞"]-3) < testTol
 
 println("Higher Order")
 Δts = 1.//2.^(7:-1:4)
 tab = constructRKF()
 sim7 = testConvergence(Δts,prob,alg="ExplicitRK",tableau=tab)
-bool7 = abs(sim7.𝒪est["l∞"]-5) < 0.1
+bool7 = abs(sim7.𝒪est["l∞"]-5) < testTol
 
 tab = constructDormandPrince()
 sim8 = testConvergence(Δts,prob,alg="ExplicitRK",tableau=tab)
-bool8 = abs(sim8.𝒪est["l∞"]-5) < 0.1
+bool8 = abs(sim8.𝒪est["l∞"]-5) < testTol
 
 tab = constructCashKarp()
 sim9 = testConvergence(Δts,prob,alg="ExplicitRK",tableau=tab)
-bool9 = abs(sim9.𝒪est["l∞"]-5) < 0.1
+bool9 = abs(sim9.𝒪est["l∞"]-5) < testTol
 
 println("Super High Order")
 #Need to make larger or else below machine ϵ
 Δts = 1./2.0.^(2:-1:-2)
 tab = constructRKF8()
 sim10 = testConvergence(Δts,prob,alg="ExplicitRK",tableau=tab)
-bool10 = abs(sim10.𝒪est["l∞"]-8) < 0.1
+bool10 = abs(sim10.𝒪est["l∞"]-8) < testTol
 
 tab = constructDormandPrince8()
 sim11 = testConvergence(Δts,prob,alg="ExplicitRK",tableau=tab)
-bool11 = abs(sim11.𝒪est["l∞"]-9.44) < 0.1 #Converges on linear at above 9!
+bool11 = abs(sim11.𝒪est["l∞"]-9.44) < testTol #Converges on linear at above 9!
 
 superbool1 = bool1 && bool2 && bool3 && bool4 && bool5 && bool6 && bool7 && bool8 && bool9 && bool10 && bool11
 
@@ -58,13 +59,13 @@ println("Convergence Test on Stiff")
 Δts = 1.//2.^(8:-1:4)
 
 sim12 = testConvergence(Δts,prob,alg="ImplicitEuler")
-bool12 = abs(sim12.𝒪est["final"]-1) < 0.1
+bool12 = abs(sim12.𝒪est["final"]-1) < testTol
 
 sim13 = testConvergence(Δts,prob,alg="Trapezoid")
-bool13 = abs(sim13.𝒪est["final"]-2) < 0.1
+bool13 = abs(sim13.𝒪est["final"]-2) < testTol
 
 sim14 = testConvergence(Δts,prob,alg="Rosenbrock32")
-bool14 = abs(sim14.𝒪est["final"]-2) < 0.1
+bool14 = abs(sim14.𝒪est["final"]-2) < testTol
 
 superbool2 = bool12 && bool13 && bool14
 
