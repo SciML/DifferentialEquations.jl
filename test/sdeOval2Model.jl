d1a0de5b63a58e771d0c565e548e532bb6b66f84
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-06-14 08:03:49 -0700

    Optimized SDE algorithms

--- test/sdeOval2Model.jl
+++ test/sdeOval2Model.jl
@@ -1,6 +1,5 @@
 addprocs(32)
-using DifferentialEquations, Plots, EllipsisNotation, JLD, LaTeXStrings, ProgressMeter
-@everywhere using Atom
+using DifferentialEquations, Plots, EllipsisNotation, JLD, LaTeXStrings
 srand(100)
 prob = oval2ModelExample(largeFluctuations=true,useBigs=false)
 
@@ -8,7 +7,9 @@ prob = oval2ModelExample(largeFluctuations=true,useBigs=false)
 
 
 #Big Run
-sol =solve(prob::SDEProblem,[0;500],Δt=(1/2)^(8),fullSave=true,alg="SRI",adaptiveAlg="RSwM3",adaptive=true,progressBar=true,saveSteps=100,abstol=1e-5,reltol=1e-3)
+sol =solve(prob::SDEProblem,[0;500],Δt=(1/2)^(8),fullSave=true,alg="SRIW1Optimized",
+          adaptiveAlg="RSwM3",adaptive=true,progressBar=true,
+          saveSteps=100,abstol=1e-5,reltol=1e-3)
 
 #Plots
 lw = 2
@@ -45,19 +46,20 @@ save("Oval2Solution.jld","sol",sol,"prob",prob)
 end
 
 ##Adaptivity Necessity Tests
-sol = solve(prob::SDEProblem,[0;1],Δt=1//2^(8),fullSave=true,alg="EM",adaptive=false,progressBar=true,saveSteps=1,abstol=1e-6,reltol=1e-4)
+sol = solve(prob::SDEProblem,[0;1],Δt=1/2^(8),fullSave=true,alg="EM",adaptive=false,progressBar=true,saveSteps=1,abstol=1e-6,reltol=1e-4)
 Int(sol.u[1]!=NaN)
 js = 18:20
 Δts = 1./2.^(js)
 fails = Array{Int}(length(Δts),2)
 times = Array{Float64}(length(Δts),2)
-numRuns = 100
+numRuns = 10000
 
 
-startTime = time()
-for j in eachindex(js)
+
+@progress for j in eachindex(js)
   println("j = $j")
   numFails = 0
+  startTime = time()
   complete = SharedArray(Int,(numRuns),init=zeros(Int,numRuns))
   t1 = @elapsed numFails = @parallel (+) for i = 1:numRuns
     sol =solve(prob::SDEProblem,[0;500],Δt=Δts[j],alg="EM")
@@ -71,8 +73,6 @@ for j in eachindex(js)
     println("\% Complete: $percentage_complete. ETA: $eta")
     flush(STDOUT)
 
-    Main.Atom.progress(sum(complete)/numRuns)
-
     Int(any(isnan,sol.u))
   end
   fails[j,1] = numFails
@@ -82,7 +82,7 @@ for j in eachindex(js)
 end
 
 
-for j in js
+@progress for j in js
   numFails = 0
   complete = SharedArray(Int,(numRuns),init=zeros(Int,numRuns))
   startTime = time()
@@ -97,9 +97,6 @@ for j in js
     eta = durationstring(eta_sec)
     println("\% Complete: $percentage_complete. ETA: $eta")
     flush(STDOUT)
-
-    Main.Atom.progress(sum(complete)/numRuns)
-
     Int(any(isnan,sol.u))
   end
   println("The number of Rossler-SRI Fails is $numFails. Elapsed time was $t2")
@@ -111,7 +108,7 @@ end
 complete = SharedArray(Int,(numRuns),init=zeros(Int,numRuns))
 startTime = time()
 adaptiveTime = @elapsed numFails = @parallel (+) for i = 1:numRuns
-  sol =solve(prob::SDEProblem,[0;1],Δt=1/2^(8),alg="SRI",adaptiveAlg="RSwM3",adaptive=true,abstol=1e-5,reltol=1e-3)
+  sol = solve(prob::SDEProblem,[0;1],Δt=1/2^(8),alg="SRIW1Optimized",adaptiveAlg="RSwM3",adaptive=true,abstol=1e-5,reltol=1e-3)
   complete[i] = 1
 
   percentage_complete = 100*(sum(complete)/numRuns)
@@ -122,7 +119,6 @@ adaptiveTime = @elapsed numFails = @parallel (+) for i = 1:numRuns
   println("\% Complete: $percentage_complete. ETA: $eta")
   flush(STDOUT)
 
-  Main.Atom.progress(sum(complete)/numRuns)
   Int(any(isnan,sol.u))
 end
 println("The number of Adaptive Fails is $numFails. Elapsed time was $adaptiveTime")
