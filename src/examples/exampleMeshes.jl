ecf86cdfd0c2fcecf35252eb53b25fdde76ef1b6
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-06-16 18:16:14 -0700

    JLD non-dependence fix

--- src/examples/exampleMeshes.jl
+++ src/examples/exampleMeshes.jl
@@ -1,25 +1,26 @@
 ###Example Meshes
+
 pkgdir = Pkg.dir("DifferentialEquations")
 "meshExample_bunny() : Returns a 3D SimpleMesh."
-meshExample_bunny() = load("$pkgdir/src/examples/prebuiltMeshes.jld","bunny")
+meshExample_bunny() = Main.JLD.load("$pkgdir/src/examples/prebuiltMeshes.jld","bunny")
 
 "meshExample_flowpastcylindermesh() : Returns a 2D SimpleMesh."
-meshExample_flowpastcylindermesh() = load("$pkgdir/src/examples/prebuiltMeshes.jld","flowpastcylindermesh")
+meshExample_flowpastcylindermesh() = Main.JLD.load("$pkgdir/src/examples/prebuiltMeshes.jld","flowpastcylindermesh")
 
 "meshExample_lakemesh() : Returns a 2D SimpleMesh."
-meshExample_lakemesh() = load("$pkgdir/src/examples/prebuiltMeshes.jld","lakemesh")
+meshExample_lakemesh() = Main.JLD.load("$pkgdir/src/examples/prebuiltMeshes.jld","lakemesh")
 
 "meshExample_Lshapemesh() : Returns a 2D SimpleMesh."
-meshExample_Lshapemesh() = load("$pkgdir/src/examples/prebuiltMeshes.jld","Lshapemesh")
+meshExample_Lshapemesh() = Main.JLD.load("$pkgdir/src/examples/prebuiltMeshes.jld","Lshapemesh")
 
 "meshExample_Lshapeunstructure() : Returns a 2D SimpleMesh."
-meshExample_Lshapeunstructure() = load("$pkgdir/src/examples/prebuiltMeshes.jld","Lshapeunstructure")
+meshExample_Lshapeunstructure() = Main.JLD.load("$pkgdir/src/examples/prebuiltMeshes.jld","Lshapeunstructure")
 
 "meshExample_oilpump() : Returns a 3D SimpleMesh."
-meshExample_oilpump() = load("$pkgdir/src/examples/prebuiltMeshes.jld","oilpump")
+meshExample_oilpump() = Main.JLD.load("$pkgdir/src/examples/prebuiltMeshes.jld","oilpump")
 
 "meshExample_wavymesh() : Returns a 2D SimpleMesh."
-meshExample_wavymesh() = load("$pkgdir/src/examples/prebuiltMeshes.jld","wavymesh")
+meshExample_wavymesh() = Main.JLD.load("$pkgdir/src/examples/prebuiltMeshes.jld","wavymesh")
 
 "meshExample_wavyperturbmesh() : Returns a 3D SimpleMesh."
-meshExample_wavyperturbmesh() = load("$pkgdir/src/examples/prebuiltMeshes.jld","wavyperturbmesh")
+meshExample_wavyperturbmesh() = Main.JLD.load("$pkgdir/src/examples/prebuiltMeshes.jld","wavyperturbmesh")
