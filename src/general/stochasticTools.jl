519e3e9e28aeaa316aa56b39621f84e23cbd0285
ChrisRackauckas <me@chrisrackauckas.com>
2016-07-04 12:28:25 -0700

    efficiency improvements

--- src/general/stochasticTools.jl
+++ src/general/stochasticTools.jl
@@ -21,8 +21,8 @@ Returns a vector of solution objects.
 * fullSave - Denotes whether fullSave should be turned on in each run. Default is true.
 * alg - Algorithm for solving the SDEs. Default is "EM"
 """
-function monteCarloSim(prob::SDEProblem;Δt::Number=0,tspan=[0,1],numMonte=10000,fullSave=true,alg="SRI",adaptive=false,abstol=1e-3,reltol=1e-2,adaptiveAlg="RSwM3")
-  elapsedTime = @elapsed solutions = pmap((i)->solve(prob,tspan,Δt=Δt,fullSave=fullSave,alg=alg,adaptive=adaptive,abstol=abstol,reltol=reltol,adaptiveAlg=adaptiveAlg),1:numMonte)
+function monteCarloSim(prob::SDEProblem;Δt::Number=0,tspan=[0,1],numMonte=10000,fullSave=false,alg="SRIW1Optimized",adaptive=false,abstol=1e-3,reltol=1e-2,adaptiveAlg="RSwM3",qmax=4)
+  elapsedTime = @elapsed solutions = pmap((i)->solve(prob,tspan,Δt=Δt,fullSave=fullSave,alg=alg,adaptive=adaptive,abstol=abstol,reltol=reltol,adaptiveAlg=adaptiveAlg,qmax=qmax),1:numMonte)
   solutions = convert(Array{SDESolution},solutions)
   if prob.knownSol
     N = size(solutions,1)
