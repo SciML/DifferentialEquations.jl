f1b7a26e2210a305e4c842714b3d9448a30d471e
ChrisRackauckas <me@chrisrackauckas.com>
2016-07-06 02:03:29 -0700

    adaptive done?

--- test/sdeAdaptiveDistributionTestPlot.jl
+++ test/sdeAdaptiveDistributionTestPlot.jl
@@ -11,6 +11,7 @@ addprocs(CPU_CORES)
   M = 20
   K = length(tols)
   ps = Array{Float64}(M,K,3)
+  qmax = 20.0
 end
 
 for k in eachindex(tols)
@@ -20,7 +21,7 @@ for k in eachindex(tols)
   println("RSwM1")
   @progress for j = 1:M
     println("j = $j")
-    Wtmp = pmap((i)->(solve(prob::SDEProblem,[0,T],Δt=1/2^(4),alg="SRIW1Optimized",adaptive=true,abstol=tol,reltol=0,adaptiveAlg="RSwM1").W),1:N)
+    Wtmp = pmap((i)->(solve(prob::SDEProblem,[0,T],Δt=1/2^(4),alg="SRIW1Optimized",adaptive=true,abstol=tol,reltol=0,adaptiveAlg="RSwM1",qmax=qmax).W),1:N)
     Wends = Float64[float(Wtmp[i]) for i in 1:N]
     kssol = ApproximateOneSampleKSTest(Wends/sqrt(T), Normal())
     ps[j,k,1] = pvalue(kssol) #Should be not significant (most of the time)
@@ -35,7 +36,7 @@ for k in eachindex(tols)
   println("RSwM2")
   @progress for j = 1:M
     println("j = $j")
-    Wtmp = pmap((i)->solve(prob::SDEProblem,[0,T],Δt=1/2^(4),alg="SRIW1Optimized",adaptive=true,abstol=tol,reltol=0,adaptiveAlg="RSwM2").W,1:N)
+    Wtmp = pmap((i)->solve(prob::SDEProblem,[0,T],Δt=1/2^(4),alg="SRIW1Optimized",adaptive=true,abstol=tol,reltol=0,adaptiveAlg="RSwM2",qmax=qmax).W,1:N)
     Wends = Float64[Wtmp[i] for i in 1:N]
     kssol = ApproximateOneSampleKSTest(Wends/sqrt(T), Normal())
     ps[j,k,2] = pvalue(kssol) #Should be not significant (most of the time)
@@ -50,7 +51,7 @@ for k in eachindex(tols)
   println("RSwM3")
   @progress for j = 1:M
     println("j = $j")
-    Wtmp = pmap((i)->solve(prob::SDEProblem,[0,T],Δt=1/2^(4),alg="SRIW1Optimized",adaptive=true,abstol=tol,reltol=0,adaptiveAlg="RSwM3").W,1:N)
+    Wtmp = pmap((i)->solve(prob::SDEProblem,[0,T],Δt=1/2^(4),alg="SRIW1Optimized",adaptive=true,abstol=tol,reltol=0,adaptiveAlg="RSwM3",qmax=qmax).W,1:N)
     Wends = Float64[Wtmp[i] for i in 1:N]
     kssol = ApproximateOneSampleKSTest(Wends/sqrt(T), Normal())
     ps[j,k,3] = pvalue(kssol) #Should be not significant (most of the time)
@@ -67,18 +68,18 @@ p = Vector{Any}(3)
 p[1] = Plots.plot(vec(repmat(tols,1,M)'),vec(ps[..,1]),xscale=:log,yscale=:log,
       linetype=:scatter,yguide="Kolmogorov Smirnov P-values",title="RSwM1",left_margin=150px,
       top_margin=50px,xticks=[1e-1,1e-3,1e-5],bottom_margin=50px,markersize=circleSize,
-      guidefont=font(guideSize),titlefont=font(titleSize),tickfont=font(tickSize),ylim=(1e-2,1),xlim=(1e-6,1),leg=false)
+      guidefont=font(guideSize),titlefont=font(titleSize),tickfont=font(tickSize),ylim=(1e-3,1),xlim=(1e-6,1),leg=false)
 
 p[2] = Plots.plot(vec(repmat(tols,1,M)'),vec(ps[..,2]),xscale=:log10,yscale=:log10,linetype=:scatter,
       ylim=(1e-2,1),xlim=(1e-6,1),title="RSwM2",xguide="Absolute Tolerance",
       top_margin=50px,xticks=[1e-1,1e-3,1e-5],bottom_margin=50px,markersize=circleSize,
-      guidefont=font(guideSize),titlefont=font(titleSize),tickfont=font(tickSize),ylim=(1e-2,1),xlim=(1e-6,1),leg=false)
+      guidefont=font(guideSize),titlefont=font(titleSize),tickfont=font(tickSize),ylim=(1e-3,1),xlim=(1e-6,1),leg=false)
 
 p[3] = Plots.plot(vec(repmat(tols,1,M)'),vec(ps[..,3]),xscale=:log10,yscale=:log10,
         linetype=:scatter,ylim=(1e-2,1),xlim=(1e-6,1),
         top_margin=50px,xticks=[1e-1,1e-3,1e-5],bottom_margin=50px,
       title="RSwM3",right_margin=30px,markersize=circleSize,
-      guidefont=font(guideSize),titlefont=font(titleSize),tickfont=font(tickSize),ylim=(1e-2,1),xlim=(1e-6,1),leg=false)
+      guidefont=font(guideSize),titlefont=font(titleSize),tickfont=font(tickSize),ylim=(1e-3,1),xlim=(1e-6,1),leg=false)
 
 Plots.plot(p[1],p[2],p[3],size=(1200,800),layout=(1,3),link=:y)
 Plots.gui()
