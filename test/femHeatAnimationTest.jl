647a3854a0a3cb2c414312c2d949c4ea1c10b83e
ChrisRackauckas <me@chrisrackauckas.com>
2016-07-11 07:26:47 -0700

    Imagemagick dependency

--- test/femHeatAnimationTest.jl
+++ test/femHeatAnimationTest.jl
@@ -4,7 +4,7 @@
 
 #Generates an animation for a solution of the heat equation
 #Uses Plots.jl, requires matplotlib >=1.5
-using DifferentialEquations
+using DifferentialEquations, Plots, ImageMagick
 T = 2
 Δx = 1//2^(3)
 Δt = 1//2^(9)
