9bf5d43a3614392f94a0fdadc6564da508d3e5e6
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2016-07-18 01:14:38 -0700

    Update fdmStokesTests.jl

--- test/fdmStokesTests.jl
+++ test/fdmStokesTests.jl
@@ -1,4 +1,6 @@
-using DifferentialEquations, LaTeXStrings
+using DifferentialEquations, LaTeXStrings, Plots
+
+## Change plot commands to GR
 
 Δx = 1//2^3 #Super large for quick testing. Lower this for better results.
 mesh = FDMMesh(Δx,mins=[-1;-1],maxs=[1;1])
@@ -10,30 +12,17 @@ prob = homogeneousStokesExample()
 err1 = sol.converrors["rul∞"]
 err2 = sol.converrors["rvl∞"]
 err3 = sol.converrors["rpl∞"]
-fig = PyPlot.figure("dgs",figsize=(10,10))
-PyPlot.xlabel("Iterations")
-PyPlot.ylabel("Error")
-PyPlot.semilogy(1:length(err1),err1)
-PyPlot.semilogy(1:length(err2),err2)
-PyPlot.semilogy(1:length(err3),err3)
-PyPlot.legend(["u","v","p"])
-texstr = L"\Delta t = 2^{-3}"
-PyPlot.title("DGS Convergence, $texstr")
+
+plot([1:length(err1) 1:length(err2) 1:length(err3)],[err1 err2 err3],yscale=:log10,xguide="Error",yguide="Iterations",title=L"DGS Convergence, $\Delta t = 2^{-3}$",label=["u" "v" "p"])
 
 #Multigrid Relative Convergence
 @time sol = solve(prob,mesh,converrors=true,maxiters=20,alg="Multigrid",levels=4)
 
+
 err1 = sol.converrors["rresul2"]
 err2 = sol.converrors["rresvl2"]
 err3 = sol.converrors["rrespl2"]
-fig = PyPlot.figure("multigrid",figsize=(10,10))
-PyPlot.xlabel("Iterations")
-PyPlot.ylabel("Error")
-PyPlot.semilogy(1:length(err1),err1)
-PyPlot.semilogy(1:length(err2),err2)
-PyPlot.semilogy(1:length(err3),err3)
-PyPlot.legend(["u","v","p"])
-texstr = L"\Delta t = 2^{-3}"
-PyPlot.title("Multigrid Relative Residual Convergence, $texstr")
+plot([1:length(err1) 1:length(err2) 1:length(err3)],[err1 err2 err3],yscale=:log10,xguide="Error",yguide="Iterations",title=L"Multigrid Relative Residual Convergence, $\Delta t = 2^{-3}$",label=["u" "v" "p"])
 
 err1[end] < 1e-12
+
