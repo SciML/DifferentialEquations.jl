ccfa4a95ea2fb3f9c1035f254d953fdfc6f08ba9
ChrisRackauckas <me@chrisrackauckas.com>
2016-07-29 16:42:58 -0700

    try this to fix the tests

---
+++ test/travis_setup.jl
@@ -0,0 +1,4 @@
+Pkg.clone("https://github.com/ChrisRackauckas/VectorizedRoutines.jl")
+Pkg.clone("https://github.com/ChrisRackauckas/ResettableStacks.jl")
+Pkg.checkout("GrowableArrays")
+VERSION < v"0.5+" && Pkg.checkout("ChunkedArrays","v0.4-compat")
