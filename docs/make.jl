c9641efa6284fd0a9d6d5ce4800cbd207f6ff166
ChrisRackauckas <me@chrisrackauckas.com>
2016-07-30 01:23:09 -0700

    documentation updates

--- docs/make.jl
+++ docs/make.jl
@@ -2,5 +2,7 @@ using Documenter, DifferentialEquations
 
 makedocs(modules=[DifferentialEquations],doctest=false)
 
-deploydocs(deps   = Deps.pip("mkdocs", "pygments", "mkdocs-material", "python-markdown-math"),
-    repo = "github.com/ChrisRackauckas/DifferentialEquations.jl.git")
+deploydocs(deps   = Deps.pip("mkdocs", "mkdocs-material", "python-markdown-math"),
+    repo = "github.com/ChrisRackauckas/DifferentialEquations.jl.git",
+    julia  = "0.5.0-",
+    osname = "linux")
