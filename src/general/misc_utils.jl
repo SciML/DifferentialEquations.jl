53d65cbc4ceeadb384294ea496883925c0467182
ChrisRackauckas <me@chrisrackauckas.com>
2016-07-29 17:08:02 -0700

    get tests to pass

--- src/general/misc_utils.jl
+++ src/general/misc_utils.jl
@@ -112,3 +112,70 @@ function monteCarlo(func::Function,args...)
     return shapeResult(res)
   end
 end
+
+"""
+meshgrid(vx)
+
+Computes an (x,y)-grid from the vectors (vx,vx).
+For more information, see the MATLAB documentation.
+"""
+meshgrid(v::AbstractVector) = meshgrid(v, v)
+
+"""
+meshgrid(vx,vy)
+
+Computes an (x,y)-grid from the vectors (vx,vy).
+For more information, see the MATLAB documentation.
+"""
+function meshgrid{T}(vx::AbstractVector{T}, vy::AbstractVector{T})
+    m, n = length(vy), length(vx)
+    vx = reshape(vx, 1, n)
+    vy = reshape(vy, m, 1)
+    (repmat(vx, m, 1), repmat(vy, 1, n))
+end
+
+"""
+meshgrid(vx,vy,vz)
+
+Computes an (x,y,z)-grid from the vectors (vx,vy,vz).
+For more information, see the MATLAB documentation.
+"""
+function meshgrid{T}(vx::AbstractVector{T}, vy::AbstractVector{T},
+                     vz::AbstractVector{T})
+    m, n, o = length(vy), length(vx), length(vz)
+    vx = reshape(vx, 1, n, 1)
+    vy = reshape(vy, m, 1, 1)
+    vz = reshape(vz, 1, 1, o)
+    om = ones(Int, m)
+    on = ones(Int, n)
+    oo = ones(Int, o)
+    (vx[om, :, oo], vy[:, on, oo], vz[om, on, :])
+end
+
+"""
+accumarray(subs, val, sz=(maximum(subs),))
+
+See MATLAB's documentation for more details.
+"""
+function accumarray(subs, val, sz=(maximum(subs),))
+    A = zeros(eltype(val), sz...)
+    for i = 1:length(val)
+        @inbounds A[subs[i]] += val[i]
+    end
+    A
+end
+
+"""
+Slower than accumarray but more functionality
+"""
+function accumarray2(subs, val, fun=sum, fillval=0; sz=maximum(subs,1), issparse=false)
+   counts = Dict()
+   for i = 1:size(subs,1)
+        counts[subs[i,:]]=[get(counts,subs[i,:],[]);val[i...]]
+   end
+   A = fillval*ones(sz...)
+   for j = keys(counts)
+        A[j...] = fun(counts[j])
+   end
+   issparse ? sparse(A) : A
+end
