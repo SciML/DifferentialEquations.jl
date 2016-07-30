729c8b43b7f4aa2302a4520584c91e553dd58f4b
ChrisRackauckas <me@chrisrackauckas.com>
2016-07-29 17:08:02 -0700

    get tests to pass

--- src/DifferentialEquations.jl
+++ src/DifferentialEquations.jl
@@ -1,27 +1,9 @@
 __precompile__()
 
-"""
-###DifferentialEquations
-
-This is a package for solving numerically solving differential equations in Julia
-by Chris Rackauckas. The purpose of this package is to supply efficient Julia
-implementations of solvers for various differential equations. Equations within
-the realm of this package include stochastic ordinary differential equations
-(SODEs or SDEs), stochastic partial differential equations (SPDEs), partial
-differential equations (with both finite difference and finite element methods),
-and differential delay equations. For ordinary differential equation solvers,
-see [ODE.jl](https://github.com/JuliaLang/ODE.jl)
-
-This package is for efficient and parallel implementations of research-level
-algorithms, many of which are quite recent. These algorithms aim to be optimized
-for HPC applications, including the use of GPUs, Xeon Phis, and multi-node
-parallelism. With the easy to use plot/convergence testing algorithms,
-this package also provides a good sandbox for developing novel numerical schemes.
-"""
 module DifferentialEquations
 
 using LaTeXStrings, IterativeSolvers, Parameters, Plots,
-      EllipsisNotation, GrowableArrays, ChunkedArrays, VectorizedRoutines
+      EllipsisNotation, GrowableArrays, ChunkedArrays
 import Base: length, size
 import JLD: load
 
