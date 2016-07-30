849c9ab681a9b774cf66403bd6731ade3fbb3560
ChrisRackauckas <me@chrisrackauckas.com>
2016-07-29 17:57:56 -0700

    get tests to pass

--- test/sde/sde_twodimlinear_tests.jl
+++ test/sde/sde_twodimlinear_tests.jl
@@ -14,18 +14,16 @@ solArr = monteCarloSim(prob::SDEProblem,Δt=1//2^(4))
 #Last indices are the indexes of the variables. Since our initial condition
 #Has 4 rows and two columns, sol.timeseries[..,1] returns the time series for the
 #first row, and sol.timeseries[..,2] returns the time series for the second.
-plot(sol,plottrue=true)
+TEST_PLOT && plot(sol,plottrue=true)
 
 ## Convergence Testing
 println("Convergence Test on 2D Linear")
 Δts = 1.//2.^(8:-1:4) #14->7 good plot
 
-println(@elapsed begin
 sim = test_convergence(Δts,prob,numMonte=5,alg=:EM)
 
 sim2 = test_convergence(Δts,prob,numMonte=5,alg=:RKMil)
 
 sim3 = test_convergence(Δts,prob,numMonte=5,alg=:SRI)
-end)
 
 abs(sim.𝒪est[:l2]-.5) + abs(sim2.𝒪est[:l∞]-1) + abs(sim3.𝒪est[:final]-1.5)<.6 #High tolerance since low Δts for testing!
