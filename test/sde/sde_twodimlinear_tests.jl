c738012ff7cf920cffe3d09aa1273479ee73294f
ChrisRackauckas <me@chrisrackauckas.com>
2016-07-29 18:14:42 -0700

    get tests to pass

--- test/sde/sde_twodimlinear_tests.jl
+++ test/sde/sde_twodimlinear_tests.jl
@@ -4,10 +4,10 @@ prob = twoDimlinearSDEExample()
 
 ## Solve and plot
 println("Solve and Plot")
-sol =solve(prob::SDEProblem,[0,1],Δt=1//2^(4),save_timeseries=true,alg=:SRIW1Optimized)
+sol =solve(prob::SDEProblem,[0,1],Δt=1/2^(3),save_timeseries=true,alg=:SRIW1Optimized)
 
 #Now do the simulation 10000 times in parallel. Return an array
-solArr = monteCarloSim(prob::SDEProblem,Δt=1//2^(4))
+solArr = monteCarloSim(prob::SDEProblem,Δt=1//2^(3),numMonte=5)
 
 #First index is the sime, so sol.timeseries[1,..] is the initial condition
 #Last indices are the indexes of the variables. Since our initial condition
@@ -17,7 +17,7 @@ TEST_PLOT && plot(sol,plottrue=true)
 
 ## Convergence Testing
 println("Convergence Test on 2D Linear")
-Δts = 1.//2.^(7:-1:4) #14->7 good plot
+Δts = 1./2.^(7:-1:4) #14->7 good plot
 
 sim = test_convergence(Δts,prob,numMonte=5,alg=:EM)
 
