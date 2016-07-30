e143ee6f07b9b7da275f7f63bbbb5870e391d000
ChrisRackauckas <me@chrisrackauckas.com>
2016-07-30 02:08:19 -0700

    deploy docs on nightly

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,4 @@ using Documenter, DifferentialEquations
 makedocs(modules=[DifferentialEquations],doctest=false)
 
 deploydocs(deps   = Deps.pip("mkdocs", "mkdocs-material", "python-markdown-math"),
-    repo = "github.com/ChrisRackauckas/DifferentialEquations.jl.git",
-    julia  = "0.5.0-",
-    osname = "linux")
+    repo = "github.com/ChrisRackauckas/DifferentialEquations.jl.git")
