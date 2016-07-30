b3e740a777c23d2cc214b8e2e810033754be6ae6
ChrisRackauckas <me@chrisrackauckas.com>
2016-07-30 13:54:20 -0700

    updated docs, tests pass

--- test/ode/ode_adaptive_tests.jl
+++ test/ode/ode_adaptive_tests.jl
@@ -18,4 +18,4 @@ sol3 =solve(prob::ODEProblem,[0,1],Δt=1/2^4,save_timeseries=true,alg=:ExplicitR
 TEST_PLOT && plot(sol3,plot_analytic=true)
 #gui()
 
-length(sol.ts)>length(sol2.ts)>=length(sol3.ts)
+length(sol.t)>length(sol2.t)>=length(sol3.t)
--- test/ode/ode_initdt_tests.jl
+++ test/ode/ode_initdt_tests.jl
@@ -6,20 +6,20 @@ println("Solve and Plot")
 tab = constructBogakiShampine()
 sol =solve(prob::ODEProblem,save_timeseries=true,alg=:ExplicitRK,adaptive=true,tableau=tab)
 TEST_PLOT && plot(sol,plot_analytic=true)
-Δt₀ = sol.ts[2]
+Δt₀ = sol.t[2]
 
 bool1 = 1e-7 < Δt₀ < .1
 
 sol =solve(prob::ODEProblem,save_timeseries=true,alg=:Euler)
 TEST_PLOT && plot(sol,plot_analytic=true)
-Δt₀ = sol.ts[2]
+Δt₀ = sol.t[2]
 
 bool2 = 1e-7 < Δt₀ < .01
 
 tab = constructDormandPrince8()
 sol3 =solve(prob::ODEProblem,save_timeseries=true,alg=:ExplicitRK,adaptive=true,tableau=tab)
 TEST_PLOT && plot(sol3,plot_analytic=true)
-Δt₀ = sol3.ts[2]
+Δt₀ = sol3.t[2]
 
 bool3 = 1e-7 < Δt₀ < .3
 
