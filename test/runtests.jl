5fbd09685c99383f502f5e40a2e365036fec2b7e
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-08-04 19:15:01 -0700

    ODE inplace tests and v0.4 compat

--- test/runtests.jl
+++ test/runtests.jl
@@ -47,6 +47,8 @@ println("ODE Number Type Tests")
 @time @test include("ode/ode_numbertype_tests.jl")
 println("ODE Initial Dt Tests")
 @time @test include("ode/ode_initdt_tests.jl")
+println("ODE In-Place Tests")
+@time @test include("ode/ode_inplace_tests.jl")
 println("ODE Feagin Tests")
 @time @test include("ode/ode_feagin_tests.jl")
 
