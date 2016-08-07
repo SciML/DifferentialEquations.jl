4af390bc76f152f0868a57e8e039a93a3fb2618c
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-08-07 10:12:18 -0700

    Fix AppVoyer ODEInterface

--- test/ci_setup.jl
+++ test/ci_setup.jl
@@ -3,5 +3,6 @@ Pkg.clone("https://github.com/JuliaODE/ODE.jl")
 Pkg.checkout("ODE","dev")
 
 if @compat is_windows()
+  println("Set GFortran Location")
   ENV["ODEINTERFACE_GFORTRAN"]="C:\mingw-w64\i686-5.3.0-posix-dwarf-rt_v4-rev0\mingw32\bin"
 end
