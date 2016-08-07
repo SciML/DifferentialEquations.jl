0c841fbb12c333d7db24aa7d029524d8deb0287b
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-08-07 10:04:30 -0700

    add ODEInterface for AppVoyer?

--- test/ci_setup.jl
+++ test/ci_setup.jl
@@ -1,2 +1,7 @@
+using Compat
 Pkg.clone("https://github.com/JuliaODE/ODE.jl")
 Pkg.checkout("ODE","dev")
+
+if @compat is_windows()
+  ENV["ODEINTERFACE_GFORTRAN"]="C:\mingw-w64\i686-5.3.0-posix-dwarf-rt_v4-rev0\mingw32\bin"
+end
