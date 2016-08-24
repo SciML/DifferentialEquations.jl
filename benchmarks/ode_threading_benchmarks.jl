53b0bcf2bca9dc59c9395bfc5ef6a8340c9e067e
ChrisRackauckas <me@chrisrackauckas.com>
2016-08-24 14:26:48 -0700

    cleaning

--- benchmarks/ode_threading_benchmarks.jl
+++ benchmarks/ode_threading_benchmarks.jl
@@ -1,7 +1,6 @@
 using DifferentialEquations
-probnum = linearODEExample()
-prob = twoDimlinearODEExample!(;α=ones(500,500),u₀=rand(500,500).*ones(500,500)/2)
-prob2 = twoDimlinearODEExample(;α=ones(500,500),u₀=rand(500,500).*ones(500,500)/2)
+probnum = prob_ode_linear
+prob = prob_ode_2Dlinear
 using BenchmarkTools
 
 elapsed1 = @elapsed sol1 =solve(prob::ODEProblem,[0,10];alg=:DP5)
