62d15a0986eea61f7a86eb02c3e05097a61a656f
ChrisRackauckas <me@chrisrackauckas.com>
2016-08-25 16:43:45 -0700

    make inplace test more robust

--- src/general/problems.jl
+++ src/general/problems.jl
@@ -456,7 +456,6 @@ numparameters(f)
 Returns the number of parameters of `f` for the method which has the most parameters.
 """
 function numparameters(f)
-  println("isgeneric: $(isgeneric(f))")
   if isgeneric(f)
     if length(methods(f))>1
       warn("Number of methods for f is greater than 1. Choosing linearity based off of method with most parameters")
