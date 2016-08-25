8638e4db308f21540143e931983fc0e134679bec
ChrisRackauckas <me@chrisrackauckas.com>
2016-08-25 10:19:38 -0700

    v0.4 robust numparameters

--- test/internals/numparameters_test.jl
+++ test/internals/numparameters_test.jl
@@ -5,7 +5,8 @@ function test_num_parameters()
   f(x,y) = 2xy
 
   numpar = numparameters(f) # Should be 2
-
-  numpar == 2
+  g = (x,y) -> x^2
+  numpar = numparameters(g)
+  numpar == 2 && numpar2 == 2
 end
 test_num_parameters()
