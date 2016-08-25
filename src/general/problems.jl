b8af88609de8034e986534907e6c76c88bf0236d
ChrisRackauckas <me@chrisrackauckas.com>
2016-08-25 16:22:20 -0700

    v0.4 robust numparameters

--- src/general/problems.jl
+++ src/general/problems.jl
@@ -456,15 +456,14 @@ numparameters(f)
 Returns the number of parameters of `f` for the method which has the most parameters.
 """
 function numparameters(f)
+  println("isgeneric: $(isgeneric(f))")
   if isgeneric(f)
     if length(methods(f))>1
       warn("Number of methods for f is greater than 1. Choosing linearity based off of method with most parameters")
     end
     numparm = maximum([length(m.sig.parameters) for m in methods(f)])
-    .
   else
-    numparm = length(Base.uncompressed_ast(f.code).args[1])
-
+    numparm = length(Base.uncompressed_ast(f.code).args[1]
   end
   if VERSION >= v"0.5-"
     return (numparm-1) #-1 in v0.5 since it adds f as the first parameter
