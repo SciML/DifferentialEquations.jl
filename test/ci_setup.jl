fa944042920ba2237d9fe505756a30969de45520
ChrisRackauckas <me@chrisrackauckas.com>
2016-09-11 23:46:50 -0700

    Sundials.jl master

--- test/ci_setup.jl
+++ test/ci_setup.jl
@@ -2,5 +2,5 @@ Pkg.clone("https://github.com/JuliaODE/ODE.jl")
 Pkg.checkout("ODE")
 
 Pkg.clone("https://github.com/alyst/Sundials.jl")
-Pkg.checkout("Sundials","handles")
+Pkg.checkout("Sundials")
 Pkg.build("Sundials")
