0e49f317018e20893f9025a5884ffbfc419d9351
ChrisRackauckas <me@chrisrackauckas.com>
2016-09-12 14:31:35 -0700

    Example updates

--- test/ci_setup.jl
+++ test/ci_setup.jl
@@ -1,6 +1,6 @@
 Pkg.clone("https://github.com/JuliaODE/ODE.jl")
 Pkg.checkout("ODE")
 
-Pkg.clone("https://github.com/JuliaMath/Sundials.jl")
+Pkg.clone("https://github.com/JuliaDiffEq/Sundials.jl")
 Pkg.checkout("Sundials")
 Pkg.build("Sundials")
