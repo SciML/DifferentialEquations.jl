77ab5d84166738af2f00a8b091b99bf3271d81df
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-10-10 22:02:27 -0700

    fix abs depwarn

--- src/fem/meshes.jl
+++ src/fem/meshes.jl
@@ -63,7 +63,7 @@ type FEMmesh <: Mesh
     ve[:,:,1] = node[elem[:,3],:]-node[elem[:,2],:]
     ve[:,:,2] = node[elem[:,1],:]-node[elem[:,3],:]
     ve[:,:,3] = node[elem[:,2],:]-node[elem[:,1],:]
-    area = 0.5*abs(-ve[:,1,3].*ve[:,2,2]+ve[:,2,3].*ve[:,1,2])
+    area = 0.5*abs.(-ve[:,1,3].*ve[:,2,2]+ve[:,2,3].*ve[:,1,2])
 
     #Boundary Conditions
     bdnode,bdedge,is_bdnode,is_bdelem = findboundary(elem)
