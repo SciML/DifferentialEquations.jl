04d39a03e28f45696eb38b82e0eb3d64cbea51a5
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-10-15 16:47:30 -0700

    Fully remove vectorized

--- src/ode/ode_constants.jl
+++ src/ode/ode_constants.jl
@@ -19,11 +19,11 @@ end
 
 const SUNDIALS_ALGORITHMS = Set([:cvode_BDF,:cvode_Adams])
 
-const DIFFERENTIALEQUATIONSJL_ALGORITHMS = Set([:Euler,:Midpoint,:RK4,:ExplicitRK,:ExplicitRKVectorized,:BS3,:BS3Vectorized,:BS5,:BS5Vectorized,:DP5,:DP5Vectorized,:DP5Threaded,:DP8,:DP8Vectorized,:Vern6,:Vern6Vectorized,:Tsit5,:Tsit5Vectorized,:TanYam7,:TanYam7Vectorized,:TsitPap8,:TsitPap8Vectorized,:Vern9,:Vern9Vectorized,:ImplicitEuler,:Trapezoid,:Rosenbrock23,:Rosenbrock32,:Feagin10,:Feagin12,:Feagin14,:Feagin10Vectorized,:Feagin12Vectorized,:Feagin14Vectorized,:Vern7,:Vern7Vectorized,:Vern8,:Vern8Vectorized])
+const DIFFERENTIALEQUATIONSJL_ALGORITHMS = Set([:Euler,:Midpoint,:RK4,:ExplicitRK,:BS3,:BS5,:DP5,:DP5Threaded,:DP8,:Vern6,:Tsit5,:TanYam7,:TsitPap8,:Vern9,:ImplicitEuler,:Trapezoid,:Rosenbrock23,:Rosenbrock32,:Feagin10,:Feagin12,:Feagin14,:Vern7,:Vern8])
 
-const DIFFERENTIALEQUATIONSJL_FSALALGS = Set([:DP5,:DP5Vectorized,:DP5Threaded,:DP8,:DP8Vectorized,:BS3,:BS3Vectorized,:BS5,:BS5Vectorized,:Tsit5,:Tsit5Vectorized,:Vern6,:Vern6Vectorized])
+const DIFFERENTIALEQUATIONSJL_FSALALGS = Set([:DP5,:DP5Threaded,:DP8,:BS3,:BS5,:Tsit5,:Vern6])
 
-const DIFFERENTIALEQUATIONSJL_SPECIALDENSEALGS = Set([:DP5,:DP5Vectorized,:DP5Threaded,:Tsit5,:Tsit5Vectorized,:BS5,:BS5Vectorized,:Vern6,:Vern6Vectorized,:Vern7,:Vern7Vectorized,:Vern8,:Vern8Vectorized,:Vern9,:Vern9Vectorized,:DP8,:DP8Vectorized]) # These algs have a special dense output, others just Hemite
+const DIFFERENTIALEQUATIONSJL_SPECIALDENSEALGS = Set([:DP5,:DP5Threaded,:Tsit5,:BS5,:Vern6,:Vern7,:Vern8,:Vern9,:DP8]) # These algs have a special dense output, others just Hemite
 const ODEINTERFACE_ALGORITHMS = Set([:dopri5,:dop853,:odex,:radau5,:radau,:seulex])
 const ODEJL_ALGORITHMS = Set([:ode23,:ode45,:ode78,:ode23s,:ode1,:ode2_midpoint,:ode2_heun,:ode4,:ode45_fe])
 
