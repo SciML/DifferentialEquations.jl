2a4f6ee9472661c1ba76fe11d439227cc2d4a7d9
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-10-16 23:32:38 -0700

    DAEProblem

--- src/general/problems.jl
+++ src/general/problems.jl
@@ -362,7 +362,7 @@ end
 """
 `ODEProblem`
 
-Wraps the data which defines an SDE problem
+Wraps the data which defines an ODE problem
 
 ```math
 \\frac{du}{dt} = f(t,u)
@@ -372,7 +372,7 @@ with initial condition ``u₀``.
 
 ### Constructors
 
-`ODEProblem(f,u₀;analytic=nothing)` : Defines the SDE with the specified functions and
+`ODEProblem(f,u₀;analytic=nothing)` : Defines the ODE with the specified functions and
 defines the solution if analytic is given.
 
 ### Fields
@@ -409,7 +409,60 @@ function ODEProblem(f::Function,u₀;analytic=nothing)
   end
   ODEProblem{typeof(u₀),eltype(u₀)}(f,u₀,analytic,knownanalytic,numvars,isinplace)
 end
-#ODEProblem{uType<:Union{AbstractArray,Number}}(f,u₀::uType,)
+
+"""
+`DAEProblem`
+
+Wraps the data which defines an DAE problem
+
+```math
+f(t,u,du₀) = 0
+```
+
+with initial conditions ``u₀`` and ``du₀``.
+
+### Constructors
+
+`DAEProblem(f,u₀,du₀;analytic=nothing)` : Defines the SDE with the specified functions and
+defines the solution if analytic is given.
+
+### Fields
+
+* `f`: The drift function in the DAE.
+* `u₀`: The initial condition.
+* `du₀`: The initial derivative.
+* `analytic`: A function which describes the solution.
+* `knownanalytic`: True if the solution is given.
+* `numvars`: The number of variables in the system
+
+"""
+type DAEProblem{uType<:Union{AbstractArray,Number},uEltype<:Number,rateType<:Union{AbstractArray,Number}} <: AbstractODEProblem
+  f::Function
+  u₀::uType
+  du₀::rateType
+  analytic::Function
+  knownanalytic::Bool
+  numvars::Int
+  isinplace::Bool
+end
+
+function DAEProblem(f::Function,u₀,du₀;analytic=nothing)
+  isinplace = numparameters(f)>=4
+  if analytic==nothing
+    knownanalytic = false
+    analytic=(t,u,du,out)->0
+  else
+    knownanalytic = true
+  end
+  if typeof(u₀) <: Number
+    sizeu = (1,)
+    numvars = 1
+  else
+    numvars = size(u₀)[end]
+  end
+  DAEProblem{typeof(u₀),eltype(u₀)}(f,u₀,du₀,analytic,knownanalytic,numvars,isinplace)
+end
+
 
 """
 `StokesProblem`
