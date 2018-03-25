8594fc4af8d5eec43cd16b5ec0e73e53b331bc6d
Christopher Rackauckas <accounts@chrisrackauckas.com>
2018-03-25 16:58:03 -0700

    Update default_dde_alg_test.jl

--- test/default_dde_alg_test.jl
+++ test/default_dde_alg_test.jl
@@ -3,9 +3,9 @@ using DifferentialEquations
 
 lags = [.2]
 f = function (u,h,p,t)
-  out = -h(t-.2) + u
+  out = -h(p,t-.2) + u
 end
-h = (t) -> 0.0
+h = (p,t) -> 0.0
 
 
 prob = DDEProblem(f,1.0,h,(0.0,10.0),constant_lags = lags)
