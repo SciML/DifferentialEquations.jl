0ea08170d8f195577c18ce7938d943b9afec32b8
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-10-02 15:22:42 +0100

    allow automatic algs from JumpProblem

--- src/default_solve.jl
+++ src/default_solve.jl
@@ -15,4 +15,13 @@ function DiffEqBase.__solve(prob::DiffEqBase.DEProblem,alg::Union{Nothing,DiffEq
   DiffEqBase.__solve(prob,alg,args...;default_set=true,kwargs...,extra_kwargs...)
 end
 
+function DiffEqBase.__solve(prob::DiffEqJump.JumpProblem,alg::Nothing,
+               args...;default_set=false,kwargs...)
+  if default_set == true
+    error("The chosen algorithm, $alg, does not exist. Please verify that the appropriate solver package has been installed.")
+  end
+  alg,extra_kwargs = default_algorithm(prob.prob;kwargs...)
+  DiffEqBase.__solve(prob,alg,args...;default_set=true,kwargs...,extra_kwargs...)
+end
+
 export default_algorithm, DefaultAlgorithm
