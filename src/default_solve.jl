15b0b60289c6a1b22694cd389b9baf4b1c5c764b
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-01-08 17:01:14 +0530

    move an __solve definition from DiffEq->DiffEqBase

--- src/default_solve.jl
+++ src/default_solve.jl
@@ -1,11 +1,3 @@
-function DiffEqBase.__solve(prob::DiffEqBase.DEProblem,args...;default_set=false,kwargs...)
-  if length(args)>0 && !(typeof(args[1]) <: DiffEqBase.DEAlgorithm)
-    error("Inappropiate solve command. The arguments do not make sense. Likely, you gave an algorithm which does not actually exist (or does not <:DiffEqBase.DEAlgorithm)")
-  else
-    DiffEqBase.__solve(prob::DiffEqBase.DEProblem,nothing,args...;default_set=false,kwargs...)
-  end
-end
-
 function DiffEqBase.__solve(prob::DiffEqBase.DEProblem,alg::Union{Nothing,DiffEqBase.DEAlgorithm},
                args...;default_set=false,kwargs...)
   if default_set == true
