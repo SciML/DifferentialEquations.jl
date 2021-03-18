b19931bf5ce2bed8c3b2c2594d89e19505c9c35c
Anand <anandj@uchicago.edu>
2021-03-18 14:45:58 -0700

    short example for module docstring

--- src/DifferentialEquations.jl
+++ src/DifferentialEquations.jl
@@ -1,3 +1,19 @@
+"""
+```julia
+using DifferentialEquations, Plots
+function lorenz!(du,u,p,t)
+ du[1] = 10.0*(u[2]-u[1])
+ du[2] = u[1]*(28.0-u[3]) - u[2]
+ du[3] = u[1]*u[2] - (8/3)*u[3]
+end
+
+u0 = [1.0;0.0;0.0]
+tspan = (0.0,100.0)
+prob = ODEProblem(lorenz!,u0,tspan)
+sol = solve(prob)
+plot(sol)
+```
+"""
 module DifferentialEquations
 
   using Reexport
