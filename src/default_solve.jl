90ae45bc861b3d047c27edacde86e2661649ead1
Chris Rackauckas <accounts@chrisrackauckas.com>
2021-08-12 00:38:06 -0400

    add prepare_alg to the solve preparation post default

--- src/default_solve.jl
+++ src/default_solve.jl
@@ -4,6 +4,7 @@ function DiffEqBase.__solve(prob::DiffEqBase.DEProblem,alg::Union{Nothing,DiffEq
     error("The chosen algorithm, $alg, does not exist. Please verify that the appropriate solver package has been installed.")
   end
   alg,extra_kwargs = default_algorithm(prob;kwargs...)
+  alg = DiffEqBase.prepare_alg(alg,prob.u0,prob.p,prob)
   DiffEqBase.__solve(prob,alg,args...;default_set=true,kwargs...,extra_kwargs...)
 end
 
