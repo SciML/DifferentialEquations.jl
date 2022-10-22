fb37868c9c1bba84c0c87f1cae657de9eb4415ec
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-10-22 11:53:44 +0200

    format

--- src/default_solve.jl
+++ src/default_solve.jl
@@ -10,14 +10,14 @@ function DiffEqBase.__solve(prob::DiffEqBase.DEProblem,
 end
 
 function DiffEqBase.__init(prob::DiffEqBase.DEProblem,
-    alg::Union{Nothing, DiffEqBase.DEAlgorithm},
-    args...; default_set = false, kwargs...)
-if default_set == true
-error("The chosen algorithm, $alg, does not exist. Please verify that the appropriate solver package has been installed.")
-end
-alg, extra_kwargs = default_algorithm(prob; kwargs...)
-alg = DiffEqBase.prepare_alg(alg, prob.u0, prob.p, prob)
-DiffEqBase.__init(prob, alg, args...; default_set = true, kwargs..., extra_kwargs...)
+                           alg::Union{Nothing, DiffEqBase.DEAlgorithm},
+                           args...; default_set = false, kwargs...)
+    if default_set == true
+        error("The chosen algorithm, $alg, does not exist. Please verify that the appropriate solver package has been installed.")
+    end
+    alg, extra_kwargs = default_algorithm(prob; kwargs...)
+    alg = DiffEqBase.prepare_alg(alg, prob.u0, prob.p, prob)
+    DiffEqBase.__init(prob, alg, args...; default_set = true, kwargs..., extra_kwargs...)
 end
 
 #=
