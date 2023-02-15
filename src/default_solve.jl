6da34f25ba0378ecd979856186062c50a93f753d
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-02-15 18:38:57 -0500

    do the conversion manually

--- src/default_solve.jl
+++ src/default_solve.jl
@@ -6,6 +6,10 @@ function DiffEqBase.__solve(prob::DiffEqBase.DEProblem,
     end
     alg, extra_kwargs = default_algorithm(prob; kwargs...)
     alg = DiffEqBase.prepare_alg(alg, prob.u0, prob.p, prob)
+
+    if alg isa SciMLBase.AbstractNonlinearAlgorithm && prob isa SciMLBase.AbstractSteadyStateProblem
+      prob = NonlinearProblem(prob)
+    end
     DiffEqBase.__solve(prob, alg, args...; default_set = true, kwargs..., extra_kwargs...)
 end
 
